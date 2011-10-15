3a12a951a35663cd1ae9de0dec50bc2123716eea
Marco Moura <email@marcomoura.com>
2011-10-15 17:18:17 -0300

    change locales

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ Catarse::Application.routes.draw do
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure" => "sessions#failure"
   match "/logout" => "sessions#destroy", :as => :logout
-  if Rails.env == "test"
+#  if Rails.env == "test"
     match "/fake_login" => "sessions#fake_create", :as => :fake_login
-  end
+#  end
   resources :projects, :only => [:index, :new, :create, :show] do
     collection do
       get 'explore'
