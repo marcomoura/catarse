96279f0f1ddf53c02321738f3d86252fc1f41c3e
Marco Moura <email@marcomoura.com>
2011-10-24 22:16:02 -0200

    fix migrate insert states

--- db/migrate/20110311091509_create_states.rb
+++ db/migrate/20110311091509_create_states.rb
@@ -11,34 +11,6 @@ class CreateStates < ActiveRecord::Migration
       t.name :not_blank => true, :unique => true
       t.acronym :not_blank => true, :unique => true
     end
-    execute "INSERT INTO states (name, acronym) VALUES
-      ('Acre', 'AC'),
-      ('Alagoas', 'AL'),
-      ('Amapá', 'AP'),
-      ('Amazonas', 'AM'),
-      ('Bahia', 'BA'),
-      ('Ceará', 'CE'),
-      ('Distrito Federal', 'DF'),
-      ('Espírito Santo', 'ES'),
-      ('Goiás', 'GO'),
-      ('Maranhão', 'MA'),
-      ('Mato Grosso', 'MT'),
-      ('Mato Grosso do Sul', 'MS'),
-      ('Minas Gerais', 'MG'),
-      ('Pará', 'PA'),
-      ('Paraíba', 'PB'),
-      ('Paraná', 'PR'),
-      ('Pernambuco', 'PE'),
-      ('Piauí', 'PI'),
-      ('Rio de Janeiro', 'RJ'),
-      ('Rio Grande do Norte', 'RN'),
-      ('Rio Grande do Sul', 'RS'),
-      ('Rondônia', 'RO'),
-      ('Roraima', 'RR'),
-      ('Santa Catarina', 'SC'),
-      ('São Paulo', 'SP'),
-      ('Sergipe', 'SE'),
-      ('Tocantins', 'TO');"
   end
 
   def self.down
