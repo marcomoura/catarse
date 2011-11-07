d09ec1ee1fc68e15f0466eda7fa680654d4758da
Marco Moura <email@marcomoura.com>
2011-11-06 22:55:30 -0200

    remove execute from migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,10 @@ Site.find_or_create_by_name(:name => "Coletivo Social",
             :facebook => "http://www.facebook.com/pages/coletivosocial", 
             :blog => "http://blog.coletivosocial.com"
             )
+
+
+=begin    execute "INSERT INTO sites (name, title, path, host, gender, email, twitter, facebook, blog) VALUES
+    ('Catarse', 'A primeira plataforma de financiamento colaborativo de projetos criativos do Brasil', 'catarse', 'catarse.me', 'male', 'contato@catarse.me', 'Catarse_', 'http://www.facebook.com/Catarse.me', 'http://blog.catarse.me'),
+    ('Multidão', 'Produção Cultural Colaborativa', 'multidao', 'multidao.localhost', 'female', 'contato@multidao.art.br', 'multidao_art', 'http://www.facebook.com/pages/Multidaoart/139326962792941', 'http://blog.multidao.art.br')
+    "
+=end
